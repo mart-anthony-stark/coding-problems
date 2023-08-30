4c7b33f1fdb25c072f48b08b6ee442687784edcb
mart-anthony-stark <martsalazar0@gmail.com>
2023-08-30 21:50:03 +0800

    Add: merge two lists

---
+++ java/3_MergeTwoLists.java
@@ -0,0 +1,65 @@
+// Merge Two Sorted Lists
+// Easy
+
+// You are given the heads of two sorted linked lists list1 and list2.
+
+// Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+// Return the head of the merged linked list.
+
+// Example 1:
+
+// Input: list1 = [1,2,4], list2 = [1,3,4]
+// Output: [1,1,2,3,4,4]
+
+// Example 2:
+
+// Input: list1 = [], list2 = []
+// Output: []
+// Example 3:
+
+// Input: list1 = [], list2 = [0]
+// Output: [0]
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class 3_MergeTwoLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode tempNode = new ListNode(0);
+        ListNode currentNode = tempNode;
+
+        while(list1 != null && list2 != null){
+            if(list1.val < list2.val){
+                currentNode.next = list1;
+                list1 = list1.next;
+            } else {
+                currentNode.next = list2;
+                list2 = list2.next;
+            }
+
+            currentNode = currentNode.next;
+        }
+
+        if(list1 != null){
+            currentNode.next = list1;
+            list1 = list1.next;
+        }else if(list2 != null){
+            currentNode.next = list2;
+            list2 = list2.next;
+        }
+
+        return tempNode.next;
+    }
+}
+
+
+// Runtime: 0 ms
+// Memory: 41.2 MB
