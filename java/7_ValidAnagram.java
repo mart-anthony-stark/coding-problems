b0d6416f44d6838c53fbc73391511105e0d13c4d
mart-anthony-stark <martsalazar0@gmail.com>
2023-09-07 21:57:34 +0800

    Add: valid anagram

---
+++ java/7_ValidAnagram.java
@@ -0,0 +1,17 @@
+class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        int[] charCount = new int[26];
+        for(int i = 0; i < s.length(); i++){
+            charCount[s.charAt(i) - 'a']++;
+            charCount[t.charAt(i) - 'a']--;
+        }
+
+        for(int c:charCount){
+            if(c != 0) return false;
+        }
+
+        return true;
+    }
+}
